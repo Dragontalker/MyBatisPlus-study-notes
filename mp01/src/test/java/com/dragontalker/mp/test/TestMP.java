35dff24d732ff5cc786972cd941d15707d891bca
Richard <richard.yang.tong@gmail.com>
2021-07-14 17:55:46 -0400

    Added notes.

---
+++ mp01/src/test/java/com/dragontalker/mp/test/TestMP.java
@@ -0,0 +1,14 @@
+package com.dragontalker.mp.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMP {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
