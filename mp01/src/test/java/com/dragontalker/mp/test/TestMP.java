8f26cb6dfcabc3f8f96b5cc9c96057fc04c0af1f
Richard <richard.yang.tong@gmail.com>
2021-07-14 17:57:37 -0400

    Added notes.

--- mp01/src/test/java/com/dragontalker/mp/test/TestMP.java
+++ mp01/src/test/java/com/dragontalker/mp/test/TestMP.java
@@ -3,12 +3,18 @@ package com.dragontalker.mp.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestMP {
+	
+	private ApplicationContext ioc = 
+			new ClassPathXmlApplicationContext("applicationContext.xml");
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDataSource() {
+		
+	
 	}
 
 }
