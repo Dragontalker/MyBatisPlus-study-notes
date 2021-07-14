cd6645c8968c9f7ecab58854771b90813410a9df
Richard <richard.yang.tong@gmail.com>
2021-07-14 17:24:59 -0400

    Added notes.

--- mp01/src/main/java/com/dragontalker/beans/Employee.java
+++ mp01/src/main/java/com/dragontalker/beans/Employee.java
@@ -11,4 +11,45 @@ public class Employee {
 	private String email;
 	private Integer gender;
 	private Integer age;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Integer getGender() {
+		return gender;
+	}
+	
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+	
+	public Integer getAge() {
+		return age;
+	}
+	
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
 }
