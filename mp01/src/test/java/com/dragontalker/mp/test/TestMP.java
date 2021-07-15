2df48576bbf3337817838f3f9ea84e47004f947e
Richard <richard.yang.tong@gmail.com>
2021-07-14 21:59:52 -0400

    Added notes.

--- mp01/src/test/java/com/dragontalker/mp/test/TestMP.java
+++ mp01/src/test/java/com/dragontalker/mp/test/TestMP.java
@@ -1,10 +1,7 @@
 package com.dragontalker.mp.test;
 
 import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.sql.DataSource;
 
