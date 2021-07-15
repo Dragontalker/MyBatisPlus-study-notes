d5bf3be39586796522fa1c1fbc5bab18f620a75a
Richard <richard.yang.tong@gmail.com>
2021-07-14 20:19:25 -0400

    Added notes.

--- mp01/src/main/java/com/dragontalker/mapper/EmployeeMapper.java
+++ mp01/src/main/java/com/dragontalker/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package com.dragontalker.mapper;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.dragontalker.beans.Employee;
 
 /**
  * Mapper接口
@@ -8,7 +9,8 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
  * 基于MyBatis: 在Mapper接口中编写CRUD相关方法 提供Mapper接口所对应的SQL映射文件以及方法对应的SQL语句
  * 
  * 基于MyBatisPlus: 让XxxMapper接口继承BaseMapper接口即可
+ *                  BaseMapper<T>: 泛型指定的就是当前Mapper接口所操作的实体类类型
  */
-public interface EmployeeMapper extends BaseMapper<T>{
+public interface EmployeeMapper extends BaseMapper<Employee>{
 
 }
