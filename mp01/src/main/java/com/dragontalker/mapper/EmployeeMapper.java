947a68b0e0ca2a36deb037bb9ff749dc5f998145
Richard <richard.yang.tong@gmail.com>
2021-07-14 20:18:30 -0400

    Added notes.

--- mp01/src/main/java/com/dragontalker/mapper/EmployeeMapper.java
+++ mp01/src/main/java/com/dragontalker/mapper/EmployeeMapper.java
@@ -1,5 +1,12 @@
 package com.dragontalker.mapper;
 
+/**
+ * Mapper接口
+ * 
+ * 基于MyBatis: 在Mapper接口中编写CRUD相关方法 提供Mapper接口所对应的SQL映射文件以及方法对应的SQL语句
+ * 
+ * 基于MyBatisPlus: 让XxxMapper接口继承BaseMapper接口即可
+ */
 public interface EmployeeMapper {
 
 }
